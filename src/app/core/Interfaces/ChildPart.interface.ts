c0b6da38f49c524a81a6d27a9cfaae728a52e9a9
roziya1326 <rosiya.mails@gmail.com>
2024-10-05 19:28:16 +0530

    newcommit:part new coponent

---
+++ src/app/core/Interfaces/ChildPart.interface.ts
@@ -0,0 +1,19 @@
+export interface ChildPart {
+    partId: number;
+    internalPartNumber: string;
+    supplierName: string;
+    deliverySiteName: string;
+    drawingNumber: string;
+    incoTerms: string;
+    annualVolume: number;
+    bomQty: number;
+    deliveryFrequency: number;
+    lotSize: number;
+    manufacturingCategory: string;
+    packagingType: string;
+    productLifeRemaining: number;
+    paymentTerms: string;
+    lifetimeQuantityRemaining: number;
+    projectId: number;
+    parentId: number;
+  }
