d4d05d5928f3b6c75b3f738259244f5cf05143ad
roziya1326 <rosiya.mails@gmail.com>
2024-10-15 06:32:24 +0530

    added:lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,16 @@
 import { Routes } from '@angular/router';
-import { NavbarComponent } from './components/nav-bar/navbar.component';
 import { CostPageComponent } from './pages/Costing/cost-page/cost-page.component';
 import { HomePageComponent } from './pages/home/home-page/home-page.component';
 
 export const routes: Routes = [
-
-      {
-        path: 'home',
-        component: HomePageComponent,
-        children:[]
-      },
-      {
-        path: 'costing',
-        component: CostPageComponent,
-        children: [],
-    },  
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'costing',
+    loadChildren: () =>
+      import('./pages/Costing/costing.module').then((m) => m.CostingModule),
+  },
 ];
